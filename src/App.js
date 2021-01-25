bd9cfc35acc31de994e227597acff6e9d2353259
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-01-24 19:58:01 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="header">
+          <h2 className="title">Take a picture</h2>
+          <img src="https://cdn.pixabay.com/photo/2020/09/19/12/49/woman-5584374__480.jpg" alt="art"/>
+        </div>
+        <div className="middle">
+          <span>Artsy Summer Collection 2020</span>
+        </div>
+        <div className="footer">
+          <p>Come to the launch on March 3, 2020. Anywhere, Any City | 10am to 6pm</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
